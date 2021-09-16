a8735b59f9fd3aab3c76774ad02b5dd35da6686b
Liyan Chen <liyanc@acm.org>
2021-09-16 18:33:28 -0500

    dataloader

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,4 +1,5 @@
 import torch
+import torchvision.transforms as T
 from torch.utils import data
 from torchvision import datasets
 
@@ -6,13 +7,18 @@ from torchvision import datasets
 __all__ = ["get_dataloder"]
 
 
-def get_dataloder(name, batch_size):
+def get_dataloder(name, batch_size, storage_path):
+    trans = T.Compose([T.ToTensor()])
     if name == "CIFAR-100":
-        train_set = datasets.CIFAR100(root="./data", train=True, download=True)
-        test_set = datasets.CIFAR100(root="./data", train=False, download=True)
+        train_set = datasets.CIFAR100(root=storage_path, train=True, download=True,
+                                      transform=T.ToTensor())
+        test_set = datasets.CIFAR100(root=storage_path, train=False, download=True,
+                                      transform=T.ToTensor())
     elif name == "STL10":
-        train_set = datasets.STL10(root="./data", split="train", download=True)
-        test_set = datasets.STL10(root="./data", split="test", download=True)
+        train_set = datasets.STL10(root=storage_path, split="train", download=True,
+                                   transform=T.ToTensor())
+        test_set = datasets.STL10(root=storage_path, split="test", download=True,
+                                      transform=T.ToTensor())
 
     return data.DataLoader(train_set, batch_size=batch_size, shuffle=True), \
            data.DataLoader(test_set, batch_size=batch_size, shuffle=False)
