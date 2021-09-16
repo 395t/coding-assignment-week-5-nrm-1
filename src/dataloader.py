fb8e966a943a7bfef80a000ecbcea7b5cac893d1
Liyan Chen <liyanc@acm.org>
2021-09-16 17:05:19 -0500

    data

---
+++ src/dataloader.py
@@ -0,0 +1,15 @@
+import torch
+from torchvision.utils import data
+from torchvision import datasets
+
+
+def get_dataloder(name, batch_size):
+    if name == "CIFAR-100":
+        train_set = datasets.CIFAR100(root="./data", train=True, download=True)
+        test_set = datasets.CIFAR100(root="./data", train=False, download=True)
+    elif name == "STL10":
+        train_set = datasets.STL10(root="./data", split="train", download=True)
+        test_set = datasets.STL10(root="./data", split="test", download=True)
+
+    return data.DataLoader(train_set, batch_size=batch_size, shuffle=True), \
+           data.DataLoader(test_set, batch_size=batch_size, shuffle=False)
