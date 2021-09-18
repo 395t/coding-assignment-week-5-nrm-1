f6d7a14aaa8a919e8e19fdae21c73d3510d1f2fe
Zayne-sprague <zayne_sprague@yahoo.com>
2021-09-18 12:49:36 -0500

    My own weight norm

--- src/normalization_layers/weight_normalization.py
+++ src/normalization_layers/weight_normalization.py
@@ -3,6 +3,7 @@ import torch
 from torch import nn
 import torch.nn.functional as F
 
+
 from functools import partial
 
 from src.paths import CHECKPOINTS_DIR, DATA_DIR
@@ -13,12 +14,78 @@ from src.viz_helper import compare_training_stats, save_plt
 
 # An example of a custom normalization layer
 class WeightNorm(nn.Module):
-    def __init__(self, layer: nn.Module):
+    def __init__(self, layer: nn.Module, weight_name: str = "weight", dim: int = 0):
         super(WeightNorm, self).__init__()
-        self.layer = layer
+
+        self.weight_name = weight_name
+        self.dim = dim
+
+        self.layer: nn.Module = layer
+
+        self._setup()
+
+    def _setup(self):
+        # Fetch the weights for the current layer passed in
+        # This weight is what we are re-parameterizing
+        # In order to do this correctly we have to remove the weight matrix from pytorches internal table of weights
+        # that can be back propped against, and add our new G and V weights instead
+
+        # Modules weights we are going to reparam.
+        weight = getattr(self.layer, self.weight_name)
+
+        # Remove the weights from the models parameter list.  This will mean it won't calc backprop on it later
+        if self.weight_name in self.layer._parameters:
+            del self.layer._parameters[self.weight_name]
+
+
+        # Initialize g
+        # From the paper we have
+        # g = ||w||
+
+        g = torch.linalg.norm(weight)
+
+        # Initialize v
+        # From the paper we have
+        # w/g = v / ||v||
+
+        # Really, we are not calculating v here.  Instead we are calculating the value v / ||v|| for the initialization
+        # although when we call forward, we will use v / ||v||
+
+        v = weight / g
+
+        # We need to register the new parameters g and v to our model.  Following some best practices we will use
+        # the same name as the weight we are re-parameterizing but with a suffix of _g or _v according to the param
+        self.layer.register_parameter(f'{self.weight_name}_g', nn.Parameter(g.data))  # g.data to avoid references to w
+        self.layer.register_parameter(f'{self.weight_name}_v', nn.Parameter(v.data))
+
+        self.set_W()
+
+
+    @staticmethod
+    def get_weight(g: torch.Tensor, v: torch.Tensor, dim: int):
+        return (g / v.norm(dim=dim, keepdim=True)) * v
+
+    @property
+    def g(self):
+        # quick helper for getting the g vector -> self.g == g vector
+        return getattr(self.layer, f'{self.weight_name}_g')
+
+    @property
+    def v(self):
+        # quick helper for getting the v matrix -> self.v == v matrix
+        return getattr(self.layer, f'{self.weight_name}_v')
+
+    def set_W(self):
+        # This function will reparam the weight matrix of the currently stored layer.
+
+        W = self.get_weight(self.g, self.v, self.dim)
+        # For the layer we are going to pass input into, we want to reset the weight matrix we are normalizing to our
+        # new parameterization
+        setattr(self.layer, self.weight_name, W)
 
     def forward(self, x):
-        #TODO implement my own weight norm
+        # For every forward call, we need to reparam the weight matrix.
+        self.set_W()
         return self.layer(x)
 
 
@@ -121,11 +188,11 @@ class ConvPoolCNNC(nn.Module):
 
 
 if __name__ == "__main__":
-    EPOCHS = 20
+    EPOCHS = 3
     BATCH_SIZE = 64
 
     net_cpc_w_norm = ConvPoolCNNC(normalizer=nn.utils.weight_norm)
-    net_cpc_wo_norm = ConvPoolCNNC(normalizer=nn.Sequential)
+    net_cpc_wo_norm = ConvPoolCNNC(normalizer=WeightNorm)
 
     # I copied the backbone and modified it slightly to work for my paper (I have to have access to the layer,
     # others may not need this though).
@@ -134,10 +201,10 @@ if __name__ == "__main__":
 
     # Different models with some parameters I want to compare against
     configs = [
-        {'name': 'Conv Pool C with Weight Norm', 'model': net_cpc_w_norm, 'save_model': 'net_cpc_w_norm', 'save_stats': 'net_cpc_w_norm_training'},
+        # {'name': 'Conv Pool C with Weight Norm', 'model': net_cpc_w_norm, 'save_model': 'net_cpc_w_norm', 'save_stats': 'net_cpc_w_norm_training'},
         {'name': 'Conv Pool C with out Weight Norm', 'model': net_cpc_wo_norm, 'save_model': 'net_cpc_wo_norm', 'save_stats': 'net_cpc_wo_norm_training'},
-        {'name': 'Backbone with Weight Norm', 'model': net_bb_w_norm, 'save_model': 'net_bb_w_norm', 'save_stats': 'net_bb_w_norm_training'},
-        {'name': 'Backbone with out Weight Norm', 'model': net_bb_wo_norm, 'save_model': 'net_bb_wo_norm', 'save_stats': 'net_bb_wo_norm_training'}
+        # {'name': 'Backbone with Weight Norm', 'model': net_bb_w_norm, 'save_model': 'net_bb_w_norm', 'save_stats': 'net_bb_w_norm_training'},
+        # {'name': 'Backbone with out Weight Norm', 'model': net_bb_wo_norm, 'save_model': 'net_bb_wo_norm', 'save_stats': 'net_bb_wo_norm_training'}
     ]
 
     # Train each model
@@ -154,28 +221,28 @@ if __name__ == "__main__":
         # Train the network on the Adam optimizer, using the training data loader, for EPOCHS epochs.
         stats = train(net, optimizer, train_dataloader, epochs=EPOCHS, loader_description=config['name'])
 
-        # Save the model for testing later
-        save_model(net, config['save_model'])
-        # Save the stats from the training loop for later
-        save_stats(stats, config['save_stats'])
+        # # Save the model for testing later
+        # save_model(net, config['save_model'])
+        # # Save the stats from the training loop for later
+        # save_stats(stats, config['save_stats'])
 
-    # Models have run, lets plot the stats
-    all_stats = []
-    labels = []
-    for config in configs:
-        all_stats.append(load_stats(config['save_stats']))
-        labels.append(config['name'])
-
-    # For every config, plot the loss across number of epochs
-    plt = compare_training_stats(all_stats, labels)
-    save_plt(plt, 'wn_training_loss_w_and_wo_test_bb_and_cnc')
-    # plt.show WILL WIPE THE PLT, so make sure you save the plot before you show it
-    plt.show()
-
-    # For every config, plot the accuracy across the number of epochs
-    plt = compare_training_stats(all_stats, labels, metric_to_compare='accuracy', y_label='accuracy', title='Accuracy vs Epoch')
-    save_plt(plt, 'wn_training_accuracy_w_and_wo_test_bb_and_cnc')
-    # plt.show WILL WIPE THE PLT, so make sure you save the plot before you show it
-    plt.show()
+    # # Models have run, lets plot the stats
+    # all_stats = []
+    # labels = []
+    # for config in configs:
+    #     all_stats.append(load_stats(config['save_stats']))
+    #     labels.append(config['name'])
+    #
+    # # For every config, plot the loss across number of epochs
+    # plt = compare_training_stats(all_stats, labels)
+    # save_plt(plt, 'wn_training_loss_w_and_wo_test_bb_and_cnc')
+    # # plt.show WILL WIPE THE PLT, so make sure you save the plot before you show it
+    # plt.show()
+    #
+    # # For every config, plot the accuracy across the number of epochs
+    # plt = compare_training_stats(all_stats, labels, metric_to_compare='accuracy', y_label='accuracy', title='Accuracy vs Epoch')
+    # save_plt(plt, 'wn_training_accuracy_w_and_wo_test_bb_and_cnc')
+    # # plt.show WILL WIPE THE PLT, so make sure you save the plot before you show it
+    # plt.show()
 
 
