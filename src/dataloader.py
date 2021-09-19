da664dba03c110f6e06089af06757d67e62c6852
Zayne Sprague <zayne_sprague@yahoo.com>
2021-09-19 12:36:35 -0500

    Fixed tiny image net

--- src/dataloader.py
+++ src/dataloader.py
@@ -2,9 +2,9 @@ import torch
 import torchvision.transforms as T
 from torch.utils import data
 from torchvision import datasets
-
+from src.paths import DATA_DIR
 import os
-from tiny_img import download_tinyImg200
+from tiny_img import download_tinyImg200, load_tiny_image
 
 __all__ = ["get_dataloder"]
 
@@ -23,10 +23,29 @@ def get_dataloder(name, batch_size, storage_path):
                                       transform=T.ToTensor())
     elif name == "TINY":
         # source: https://colab.research.google.com/github/yandexdataschool/mlhep2019/blob/master/notebooks/day-3/seminar_convnets.ipynb#scrollTo=tvz-gycUrYD1
-        if not os.path.exists('./tiny-imagenet-200/'):
-            download_tinyImg200('.')
-        train_set = datasets.ImageFolder('tiny-imagenet-200/train', transform=T.ToTensor())
-        test_set = datasets.ImageFolder('tiny-imagenet-200/val', transform=T.ToTensor())
+        if not os.path.exists(str(DATA_DIR / 'tiny-imagenet-200')):
+            download_tinyImg200()
+        train_set = datasets.ImageFolder(str(DATA_DIR / 'tiny-imagenet-200/train'), transform=T.ToTensor())
+        test_set = load_validation_tinynet(train_set)
 
     return data.DataLoader(train_set, batch_size=batch_size, shuffle=True), \
            data.DataLoader(test_set, batch_size=batch_size, shuffle=False)
+
+
+def load_validation_tinynet(train_set: datasets.ImageFolder) -> datasets.ImageFolder:
+    tinynet_path = DATA_DIR / 'tiny-imagenet-200'
+    validation_path = tinynet_path / 'val'
+    validation_file = validation_path / 'val_annotations.txt'
+
+    val_file = open(str(validation_file), 'r')
+    val_lines = val_file.readlines()
+
+    targets = []
+    for line in val_lines:
+        target_class = line.split("\t")[1]
+        target_idx = train_set.classes.index(target_class)
+        targets.append(target_idx)
+
+    validation_set = datasets.ImageFolder(str(DATA_DIR / 'tiny-imagenet-200/val'), transform=T.ToTensor())
+    validation_set.targets = targets
+    return validation_set
