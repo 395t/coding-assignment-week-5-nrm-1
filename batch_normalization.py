65cd5cfbf6459e917d21242b583dfab4e439d49d
Kelsey Ball <kelseytaylorball@gmail.com>
2021-09-18 19:11:18 -0500

    Finish experiment runner
    - Add tiny image net
    - Plot results
    
    Test plan: ran 1 batch, 1 epoch version of each dataset and verify that metrics/plots are saved out

--- batch_normalization.py
+++ batch_normalization.py
@@ -5,7 +5,8 @@ from torch import nn
 from functools import partial
 
 from src.paths import CHECKPOINTS_DIR, DATA_DIR
-from src.lifecycles import train, test, test_validation, save_model, load_modal
+from src.lifecycles import train, test, test_validation, save_model, load_modal, save_stats, load_stats
+from src.viz_helper import compare_training_stats, save_plt
 
 
 # No-op normalization layer
@@ -13,54 +14,81 @@ class MyNormLayer(nn.Module):
     def __init__(self, extra_parameter_saved_via_partial):
         super(MyNormLayer, self).__init__()
 
-        # This parameter is passed in via the partial(module, arg1, arg2, arg3, ...) call
-        print(extra_parameter_saved_via_partial)
-
-        # more stuff here
-
     def forward(self, x):
-        # more stuff here
         return x
 
 
 if __name__ == "__main__":
-    # Experiments to reproduce from batch norm paper
-    experiments = {
-        'baseline': {'mod': MyNormLayer, 'lr': 0.001},
-        'batch_norm': {'mod': nn.BatchNorm2d, 'lr': 0.001},
-        'batch_norm_5x': {'mod': nn.BatchNorm2d, 'lr': 0.005},
-        'batch_norm_30x': {'mod': nn.BatchNorm2d, 'lr': 0.030},
-    }
-    # Grab the CIFAR-100 dataset, with a batch size of 10, and store it in the Data Directory (src/data)
-    train_dataloader, test_dataloader = src.get_dataloder('CIFAR-100', 10, DATA_DIR)
 
-    for exp_name, config in experiments.items():
-        print(f'---Running experiment {exp_name}---')
-        norm_mod = partial(config['mod'], 128)
+    # todo: add third dataset
+    for dataset, num_classes in (('STL10', 100), ('TINY', 200)):
+        # Experiments to reproduce from batch norm paper
+        experiments = {
+            'baseline': {'mod': MyNormLayer, 'lr': 0.001},
+            # 'batch_norm': {'mod': nn.BatchNorm2d, 'lr': 0.001},
+            # 'batch_norm_5x': {'mod': nn.BatchNorm2d, 'lr': 0.005},
+            # 'batch_norm_30x': {'mod': nn.BatchNorm2d, 'lr': 0.030},
+        }
+        # Grab the dataset, with a batch size of 10, and store it in the Data Directory (src/data)
+        train_dataloader, test_dataloader = src.get_dataloder(dataset, 10, DATA_DIR)
 
-        # Create the backbone network with 100 classes and the new MyNormLayer normilization layer
-        net = src.Backbone(100, norm_mod)
+        for exp_name, config in experiments.items():
+            print(f'---Running experiment {exp_name} on dataset {dataset} ---')
+            norm_mod = partial(config['mod'], 128)
 
-        # Set up a learning rate and optimizer
-        LR = config['lr']
-        optimizer = torch.optim.Adam(net.parameters(), lr=LR)
+            # Create the backbone network with 100 classes and batch norm
+            net = src.Backbone(num_classes, norm_mod)
 
-        # Train the network on the Adam optimizer, using the training data loader, for 10 epochs
-        print('---Training---')
-        train_metrics = train(net, optimizer, train_dataloader, epochs=10)
+            # Set up a learning rate and optimizer
+            LR = config['lr']
+            optimizer = torch.optim.Adam(net.parameters(), lr=LR)
 
-        with open(str(CHECKPOINTS_DIR / f'{exp_name}_train_metrics.pkl'), mode='wb') as f:
-            pkl.dump(train_metrics, f)
+            # Train the network on the Adam optimizer, using the training data loader, for 10 epochs
+            print('---Training---')
 
-        # Save the model for use later in the checkpoints directory (src/checkpoints) as 'example_model.pt'
-        save_model(net, exp_name)
+            # can use below for quick test
+            # train_metrics = train(net, optimizer, list(train_dataloader)[:1], epochs=2)
+            train_metrics = train(net, optimizer, train_dataloader, epochs=10)
 
-        # load the model from the saved file
-        net = load_modal(exp_name)
+            save_path = f'{dataset}_{exp_name}_train_metrics'
+            save_stats(train_metrics, save_path)
 
-        # Test the model on the test dataloader
-        print('---Validation---')
-        val_metrics = test_validation(net, test_dataloader)
+            # Save the model for use later in the checkpoints directory (src/checkpoints) as 'example_model.pt'
+            save_model(net, exp_name)
 
-        with open(str(CHECKPOINTS_DIR / f'{exp_name}_val_metrics.pkl'), mode='wb') as f:
-            pkl.dump(val_metrics, f)
+            # load the model from the saved file
+            net = load_modal(exp_name)
+
+            # Test the model on the test dataloader
+            print('---Validation---')
+
+            # can use below for quick test
+            # val_metrics = test_validation(net, list(test_dataloader)[:1])
+            val_metrics = test_validation(net, test_dataloader)
+
+            save_path = f'{dataset}_{exp_name}_val_metrics'
+            save_stats(val_metrics, save_path)
+
+
+        # Models have run, lets plot the stats
+        all_stats = []
+        labels = []
+        for exp_name in experiments.keys():
+            load_path = f'{dataset}_{exp_name}_train_metrics'
+            all_stats.append(load_stats(load_path))
+            labels.append(exp_name)
+
+        # For every config, plot the loss across number of epochs
+        plt = compare_training_stats(all_stats, labels)
+        save_plt(plt, f'{dataset}_loss_train')
+        # plt.show WILL WIPE THE PLT, so make sure you save the plot before you show it
+        plt.show()
+
+        # aggregate test accuracies across experiments and save
+        test_acc = {}
+        for exp_name in experiments.keys():
+            load_path = f'{dataset}_{exp_name}_val_metrics'
+            stats = load_stats(load_path)
+            exp = f'{dataset}_{exp_name}'
+            test_acc[exp] = stats['accuracy']
+        save_stats(test_acc, f'{dataset}_test_acc')
