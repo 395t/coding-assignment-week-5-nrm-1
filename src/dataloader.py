1faefba9399f2b4821fa0a118740df29564495da
Kelsey Ball <kelseytaylorball@gmail.com>
2021-09-18 19:13:56 -0500

    Add TinyImageNet
    - Added helper script for downloading (URL/source provided in file header)
    - Updated `get_dataloder` to return train/test splits when you pass in "TINY" as the dataset name
    
    Test plan: trained a tiny version of my model using the returned splits

--- src/dataloader.py
+++ src/dataloader.py
@@ -3,6 +3,8 @@ import torchvision.transforms as T
 from torch.utils import data
 from torchvision import datasets
 
+import os
+from tiny_img import download_tinyImg200
 
 __all__ = ["get_dataloder"]
 
@@ -19,6 +21,12 @@ def get_dataloder(name, batch_size, storage_path):
                                    transform=T.ToTensor())
         test_set = datasets.STL10(root=storage_path, split="test", download=True,
                                       transform=T.ToTensor())
+    elif name == "TINY":
+        # source: https://colab.research.google.com/github/yandexdataschool/mlhep2019/blob/master/notebooks/day-3/seminar_convnets.ipynb#scrollTo=tvz-gycUrYD1
+        if not os.path.exists('./tiny-imagenet-200/'):
+            download_tinyImg200('.')
+        train_set = datasets.ImageFolder('tiny-imagenet-200/train', transform=T.ToTensor())
+        test_set = datasets.ImageFolder('tiny-imagenet-200/val', transform=T.ToTensor())
 
     return data.DataLoader(train_set, batch_size=batch_size, shuffle=True), \
            data.DataLoader(test_set, batch_size=batch_size, shuffle=False)
