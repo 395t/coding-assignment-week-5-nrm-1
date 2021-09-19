b1c5b1a6cba87208218974b122f40846c32788af
Zayne Sprague <zayne_sprague@yahoo.com>
2021-09-19 12:44:13 -0500

    Notes on what the helper func does for tiny image net

--- src/dataloader.py
+++ src/dataloader.py
@@ -33,19 +33,37 @@ def get_dataloder(name, batch_size, storage_path):
 
 
 def load_validation_tinynet(train_set: datasets.ImageFolder) -> datasets.ImageFolder:
+    # TinyImage net is annotated the same way for train and val folders.
+    # Train has each class separated into subfolders, however, val has each class annotated in a separate text file
+    # Without this helper function, loading the validation folder as an imagefolder will cause all examples to have
+    # a target class of 0
+
+    # Set up some nice path constants
     tinynet_path = DATA_DIR / 'tiny-imagenet-200'
     validation_path = tinynet_path / 'val'
     validation_file = validation_path / 'val_annotations.txt'
 
+    # Read from the validation annotation file
     val_file = open(str(validation_file), 'r')
     val_lines = val_file.readlines()
 
+    # These will be our new targets
     targets = []
+
+    # Read line by line (each line is one example, in order)
     for line in val_lines:
+
+        # Split the line by the files delimiters (tabs)
         target_class = line.split("\t")[1]
+
+        # The train set will have a mapping of "class name" to "idx".
+        # IMPORTANT: use whats in the trainset not the order given from tinynets wnids.txt (they will be out of sync)
         target_idx = train_set.classes.index(target_class)
         targets.append(target_idx)
 
+    # Load the validation image folder
     validation_set = datasets.ImageFolder(str(DATA_DIR / 'tiny-imagenet-200/val'), transform=T.ToTensor())
+
+    # Overwrite the bad targets with our new ones.
     validation_set.targets = targets
     return validation_set
